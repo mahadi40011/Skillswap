1497422cadcaf6ad1c153826506671feb8873768
mahadi40011 <mahadi40011@gmail.com>
2025-10-25 14:55:29 +0600

    update profile form problem solved

--- src/components/UpdateProfileForm/UpdateProfileForm.jsx
+++ src/components/UpdateProfileForm/UpdateProfileForm.jsx
@@ -6,11 +6,12 @@ import { AuthContext } from "../../context/AuthContext";
 const UpdateProfileForm = ({ setUpdateProfile }) => {
   const { user, setUser, updateUserProfile } = useContext(AuthContext);
 
+  const existingPhotoUrl = user.photoURL;
+  const existingDisplayName = user.displayName;
+
   useEffect(() => {
     Aos.init();
   }, []);
-
-  const existingPhotoUrl = user.photoURL;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,19 +20,24 @@ const UpdateProfileForm = ({ setUpdateProfile }) => {
 
     if (!displayName && !photoURL)
       return toast.error("Please Provide something");
-    if (displayName.length <= 3 || !/^[A-Z]/.test(displayName))
-      return toast.error(
-        "Please Provide Valid Name. First letter is uppercase"
-      );
+    if (displayName) {
+      if (displayName.length <= 3 || !/^[A-Z]/.test(displayName))
+        return toast.error(
+          "Please Provide Valid Name. First letter is uppercase"
+        );
+    }
 
     const updatedPhotoUrl =
       photoURL.trim() !== "" ? photoURL : existingPhotoUrl;
 
-    updateUserProfile(displayName, updatedPhotoUrl)
+    const updatedDisplayName =
+      displayName.trim() !== "" ? displayName : existingDisplayName;
+
+    updateUserProfile(updatedDisplayName, updatedPhotoUrl)
       .then(() => {
         setUser(() => ({
           ...user,
-          displayName,
+          displayName: updatedDisplayName,
           photoURL: updatedPhotoUrl,
         }));
         e.target.reset();
@@ -49,37 +55,43 @@ const UpdateProfileForm = ({ setUpdateProfile }) => {
       data-aos-duration="1000"
       className="flex justify-center mt-8"
     >
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white shadow-lg p-6 rounded-2xl w-80"
-      >
-        <h2 className="text-xl font-semibold mb-4 text-center">
-          Update Your Profile
-        </h2>
+      <div className="bg-white shadow-lg p-6 rounded-2xl w-80">
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-xl font-semibold mb-4 text-center">
+            Update Your Profile
+          </h2>
 
-        <label className="block mb-2 font-medium">Name</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Your Name"
-          className="w-full mb-4 border rounded-lg px-3 py-1.5 outline-none focus:ring-2 focus:ring-sky-900"
-        />
+          <label className="block mb-2 font-medium">Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            className="w-full mb-4 border rounded-lg px-3 py-1.5 outline-none focus:ring-2 focus:ring-sky-900"
+          />
 
-        <label className="block mb-2 font-medium">PhotoURL</label>
-        <input
-          type="text"
-          name="photo"
-          placeholder="PhotoURL"
-          className="w-full mb-4 border rounded-lg px-3 py-1.5 outline-none focus:ring-2 focus:ring-sky-900"
-        />
+          <label className="block mb-2 font-medium">PhotoURL</label>
+          <input
+            type="text"
+            name="photo"
+            placeholder="PhotoURL"
+            className="w-full mb-4 border rounded-lg px-3 py-1.5 outline-none focus:ring-2 focus:ring-sky-900"
+          />
 
+          <button
+            type="submit"
+            className="w-full custom-gradient font-semibold py-2 rounded-lg"
+          >
+            Submit
+          </button>
+        </form>
         <button
+          onClick={() => setUpdateProfile(false)}
           type="submit"
-          className="w-full custom-gradient font-semibold py-2 rounded-lg"
+          className="w-full custom-gradient font-semibold py-2 mt-3 rounded-lg"
         >
-          Submit
+          Back
         </button>
-      </form>
+      </div>
     </div>
   );
 };
